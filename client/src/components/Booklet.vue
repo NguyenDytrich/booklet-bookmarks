e64495fd97be0f3db46ffaf31c86e3f8cd4f1f1d
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2020-09-17 16:40:56 -0700

    Now you can delete stuff

--- client/src/components/Booklet.vue
+++ client/src/components/Booklet.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="panel">
+  <div class="panel is-primary">
     <h1 class="panel-heading">Booklet</h1>
-    <a
-      class="panel-block"
-      v-for="i in bookmarkMap"
-      :key="i.id"
-      @click="openInNew(i.bk.link)"
-    >
-      {{ i.bk.name }}
-    </a>
+    <div v-for="i in bookmarkMap" :key="i.id" class="panel-block">
+      <div class="bookmark">
+        <div class="bookmark-item">
+          <a @click="openInNew(i.bk.link)">
+            {{ i.bk.name }}
+          </a>
+        </div>
+        <div class="bookmark-controls">
+          <span
+            class="delete is-light is-pulled-right"
+            @click="deleteBookmark(i.id)"
+          />
+        </div>
+      </div>
+    </div>
     <div class="panel-block">
-      <form v-on:submit.prevent="onSubmit">
-        <div class="field is-grouped">
+      <form v-on:submit.prevent="onSubmit" style="width: 100%;">
+        <div class="field is-grouped is-grouped-centered">
           <div class="field">
             <input
               class="input"
@@ -71,6 +78,10 @@ export default class Booklet extends Vue {
       window.open(`//${link}`, "_blank");
     }
   }
+
+  private deleteBookmark(id: number) {
+    this.bookmarks.splice(id, 1);
+  }
 }
 </script>
 
@@ -82,15 +93,39 @@ export default class Booklet extends Vue {
       border-right: none;
     }
   }
+
   div:nth-child(2) {
     input {
       border-radius: 0;
       border-right: none;
     }
   }
+
   button {
     border-radius: 0 5px 5px 0;
     border-left: none;
+  }
+}
+
+.bookmark {
+  display: grid;
+  grid-template-columns: auto 20%;
+  width: 100%;
+  padding: 0 1em;
+
+  .bookmark-controls,
+  .bookmark-item {
+    padding: 10px 0 5px 0;
+  }
+
+  .bookmark-item {
+    grid-column-start: 0;
+    grid-column-end: 1;
+  }
+
+  .bookmark-controls {
+    grid-column-start: 2;
+    grid-column-end: -1;
   }
 }
 </style>
